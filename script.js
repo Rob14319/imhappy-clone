e44b4477e8fd0677e1103ae45da6ba50c29a68d7
Robin <roinbudewa25@gmail.com>
2025-04-27 12:24:18 +0530

    Initial commit - I'm Happy clone

---
+++ script.js
@@ -0,0 +1,22 @@
+// JS is optional for extra animations on scroll
+
+const faders = document.querySelectorAll('.fade-in, .slide-up');
+
+const appearOptions = {
+    threshold: 0.3,
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
